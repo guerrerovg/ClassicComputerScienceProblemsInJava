57eac270db1ca718e4589952f04bef1efd2812c8
guerrerovg <jesusvargasgro@gmail.com>
2023-06-20 22:29:52 -0600

    add Node class, dfs and nodeToPath methods

--- CCSPiJ/src/chapter2/GenericSearch.java
+++ CCSPiJ/src/chapter2/GenericSearch.java
@@ -1,6 +1,8 @@
 package chapter2;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class GenericSearch {
 
@@ -30,6 +32,78 @@ public class GenericSearch {
         }
 
         return false;
+    }
+
+    public static class Node<T> implements Comparable<Node<T>> {
+        final T state;
+        Node<T> parent;
+        double cost;
+        double heuristic;
+
+        // dfs, bfs
+        Node(T state, Node<T> parent) {
+            this.state = state;
+            this.parent = parent;
+        }
+
+        // a*
+        Node(T state, Node<T> parent, double cost, double heuristic) {
+            this.state = state;
+            this.parent = parent;
+            this.cost = cost;
+            this.heuristic = heuristic;
+        }
+
+        @Override
+        public int compareTo(Node<T> other) {
+            Double mine = cost + heuristic;
+            Double theirs = other.cost + other.heuristic;
+
+            return mine.compareTo(theirs);
+        }
+    }
+
+    public static <T> Node<T> dfs(T initial, Predicate<T> goalTest, Function<T, List<T>> successors) {
+        // frontier is where we've yet to go
+        Stack<Node<T>> frontier = new Stack<>();
+        frontier.push(new Node<>(initial, null));
+
+        // explored is where we've been
+        Set<T> explored = new HashSet<>();
+        explored.add(initial);
+
+        // keep going while there is more to explore
+        while (!frontier.isEmpty()) {
+            Node<T> currentNode = frontier.pop();
+            T currentState = currentNode.state;
+            if (goalTest.test(currentState)) {
+                return currentNode;
+            }
+
+            // check where we can go next and haven't explored
+            for (T child : successors.apply(currentState)) {
+                if (explored.contains(child)) {
+                    continue;
+                }
+
+                explored.add(child);
+                frontier.push(new Node<>(child, currentNode));
+            }
+        }
+
+        return null;
+    }
+
+    public static <T> List<T> nodeToPath(Node<T> node) {
+        List<T> path = new ArrayList<>();
+        path.add(node.state);
+
+        while (node.parent != null) {
+            node = node.parent;
+            path.add(0, node.state);
+        }
+
+        return path;
     }
 
     public static void main(String[] args) {
